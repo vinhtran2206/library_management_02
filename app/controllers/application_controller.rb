3eb544804ad9931f22d19b887365d0e25e930934
[DN_OE18_Ruby]TranNgocVinh <vinhtran09121997@gmail.com>
2019-01-22 09:33:18 +0700

    fix author and book

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include BorrowsHelper
 
-  def respond_modal_with(*args, &blk)
-    options = args.extract_options!
-    options[:responder] = ModalResponder
-    respond_with *args, options, &blk
+  private
+
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t ".login"
+    redirect_to login_path
+  end
+
+  def is_admin?
+    return if current_user.admin?
+    redirect_to root_path
+    flash[:danger] = t ".permission"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "carts/show"
   default_url_options host: "localhost:3000"
   root "books#index"
   get "users/show"
@@ -6,18 +7,24 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  get "/login", to: "sessions#new" #User
+  get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
-  delete "logout" => "sessions#destroy"
-  get "/newauthors", to: "authors#new" #Author
-  get "/authors", to: "authors#index"
-  get "/newpublishers", to: "publishers#new"
-  get "/publishers", to: "publishers#index"
-  get "/newbooks", to: "books#new"
-  get "/categories", to: "categories#index"
-  get "/newcategories", to: "categories#new"
-  resources :users
-  resources :authors, :publishers, :books, :categories
+  delete "logout", to: "sessions#destroy"
+
+  resources :users, :authors, :publishers, :books, :categories, :borrows
+  resources :borrow_details, only: [:create, :update, :destroy]
+  resource :cart, only: [:show]
+
+  namespace :admin do
+    resources :books
+    resources :borrows do
+      member do
+        get "accept_request"
+        patch "deny_request"
+      end
+    end
+    resources :borrow_details, only: [:create, :update, :destroy]
+  end
 end
